b356871f0aed771bdadd7766cc552d374e9a4320
mjaliz <mrph14@gmail.com>
2024-05-09 23:12:09 +0330

    019 Closing the drawer manually

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -2,7 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({
+    super.key,
+    required this.onSelectScreen,
+  });
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +50,22 @@ class MainDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: Icon(
+              Icons.fastfood,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              "Meals",
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontSize: 24),
+            ),
+            onTap: () {
+              onSelectScreen("meals");
+            },
+          ),
+          ListTile(
+            leading: Icon(
               Icons.settings,
               size: 26,
               color: Theme.of(context).colorScheme.onBackground,
@@ -55,7 +76,9 @@ class MainDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                   fontSize: 24),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen("filters");
+            },
           )
         ],
       ),
